f1a5204a2ade752681f8cc459b1f8a9efe21f4fa
ayunur4321 <ayunur7164@gmail.com>
2026-08-04 10:03:43 +0700

    PORT 5000

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('GlowList Backend API berjalan!!!');
+});
+
+app.listen(PORT,() => {
+    console.log(`server GlowList jalan di http://localhost:${PORT}`);
+
+})
